71cac0032df0ed8d028585b189d92ca7e952c79d
Dmitriy Basov <dmitrybasov_787788@mail.ru>
2024-10-04 03:43:54 +0300

    add lesson3:Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿
+void ZeroEvenElements(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+        }
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (int e in arr)
+    {
+        Console.Write($"{e} ");
+    }
+}
+
+int[] array = { 5, 8, 10, 4, 3 };
+ZeroEvenElements(array);
+PrintArray(array);
